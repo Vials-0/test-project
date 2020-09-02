9ab797eaa741f1fd10fa24664ea4124f9e07f3da
Bradley_Carson <bmcarson1671+1@gmail.com>
2020-09-02 08:58:40 -0400

    Update icon styling and font family

--- frontend/src/components/PlaceCard/PlaceCard.jsx
+++ frontend/src/components/PlaceCard/PlaceCard.jsx
@@ -6,12 +6,14 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import { green } from '@material-ui/core/colors';
 import { default as YesIcon } from '@material-ui/icons/CheckCircle';
 import { default as NoIcon } from '@material-ui/icons/Cancel';
 
 const PlaceCard = ({ place }) => {
     return (
         <Card className='card'>
+            {/* TODO: Use background image for similarly-sized images */}
             <img
                 className='card-image'
                 src={place.imageUrl}
@@ -23,15 +25,15 @@ const PlaceCard = ({ place }) => {
                     <div className='mask-required-container'>
                         <p>Masks required for customers:</p>
                         {place.requireCustomerMask
-                            ? <YesIcon className='yes-no-icon' />
-                            : <NoIcon className='yes-no-icon' />
+                            ? <YesIcon className='yes-no-icon' style={{ color: green[500], fontSize: 32 }} />
+                            : <NoIcon className='yes-no-icon' style={{ fontSize: 32 }} />
                         }
                     </div>
                     <div className='mask-required-container'>
                         <p>Masks required for employees:</p>
                         {place.requireEmployeeMask
-                            ? <YesIcon className='yes-no-icon' />
-                            : <NoIcon className='yes-no-icon' />
+                            ? <YesIcon className='yes-no-icon' style={{ color: green[500], fontSize: 32 }} />
+                            : <NoIcon className='yes-no-icon' style={{ fontSize: 32 }} />
                         }
                     </div>
                 </section>
